771db7c91eeb33b46185ec956aea89d2447517a5
Marcondes Amorimm <marcondesjra@gmail.com>
2022-01-27 23:00:11 -0300

    atualização

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,43 @@
+function GlobalStyle() {
+    return (
+        <style global jsx> {`
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            body {
+                font-family: sans-serif;
+            }
+        `}
+        </style>
+    ) 
+}
+
+function Titulo(props) {
+    const Tag = props.tag;
+    return (
+        <>
+            <Tag>{props.children}</Tag> 
+            <style jsx>{`
+                    ${Tag} {
+                        color: red;
+                        font-size: 24px;
+                        font-weight: 600;
+                    }
+                `}
+            </style>
+        </>
+    );
+}
 //Compónent React
 function HomePage() {
     //JSX
     return (
         <div>
-            <h1>Boas vindas de volta!</h1>
+            <GlobalStyle />
+            <Titulo tag="h2">Boas vindas de volta!</Titulo>
             <h2>Discord - Alura Matrix</h2>
-            <style jsx>{`
-                h1 {
-                    color: red;
-                }
-            `}</style>
         </div>
     )
 }
